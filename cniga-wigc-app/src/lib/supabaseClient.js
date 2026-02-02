fdf3753d5203504c253d1f1855b0523a8bb28168
michaelrayburke <34951495+michaelrayburke@users.noreply.github.com>
2026-02-02 09:30:34 -0800

    Delete cniga-wigc-app directory

--- cniga-wigc-app/src/lib/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import "react-native-url-polyfill/auto";
-import { createClient } from "@supabase/supabase-js";
-
-const url = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const anon = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(url, anon);
